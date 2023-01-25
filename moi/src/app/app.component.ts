5df430a6d338fa766d31a2430f0a3c61e48f008c
Bono Arts <bonoarts2000@gmail.com>
2023-01-25 19:27:36 +0000

    update

---
+++ moi/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+declare var ConfettiGenerator: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Maagd';
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
+  ngOnInit(){
+    var confettiSettings = { target: 'my-canvas' };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+
+    this.temp();
+  }
+
+  temp() {
+   // To set two dates to two variables
+   var date1 = new Date("08/11/2000");
+   var date2 = new Date();
+     
+   // To calculate the time difference of two dates
+   var Difference_In_Time = date2.getTime() - date1.getTime();
+     
+   // To calculate the no. of days between two dates
+   this.days = Math.ceil(Difference_In_Time / (1000 * 3600 * 24));
+     
+  }
+}
